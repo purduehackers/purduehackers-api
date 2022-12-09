ca35d5dda6329afa2453d39612b52c42dc0c30d8
Matthew Stanciu <mattbstanciu@gmail.com>
2022-12-09 01:13:28 -0500

    Remove typed events
    Actually I don't think it should be the job of the API to filter the request from Airtable. I think the only thing the API should do, at least for events, is make the API call to Airtable on your behalf, and return exactly what Airtable returns.

--- utils/fetchEvents.ts
+++ utils/fetchEvents.ts
@@ -1,12 +1,10 @@
 import { FieldSet, Records } from "airtable";
-import { GithubSlugger } from "github-slugger-typescript";
 import { QueryParams } from "airtable/lib/query_params";
 import { eventTable } from "../db/events";
-import PHEvent from "./interfaces/PHEvent";
 
 export async function fetchEvents(
   select?: QueryParams<FieldSet>
-): Promise<PHEvent[]> {
+): Promise<Records<FieldSet>> {
   return new Promise((resolve, reject) => {
     select.filterByFormula = `AND({Unlisted} = 0, {Event Name} != ''${
       select.filterByFormula ? `, ${select.filterByFormula}` : ""
@@ -14,44 +12,7 @@ export async function fetchEvents(
     eventTable
       .select(select)
       .all()
-      .then((events) => {
-        const slugger = new GithubSlugger();
-        const typedEvents = events.map(({ id, fields }) => ({
-          id,
-          name: fields["Event Name"],
-          desc:
-            fields["Event Description"] ??
-            `We're still working on this event...check back later for more details!`,
-          start: fields["Event Date & Start Time"] ?? "TBD",
-          end: fields["Event Date & End Time"] ?? "TBD",
-          loc: fields["Event Location"] ?? "TBD",
-          gMap: fields["Location Map Link (optional)"] ?? false,
-          calLink: fields["Calendar Link"] ?? false,
-          ogDescription: fields["OG Description"] ?? "",
-          emailSent: fields["Reminder Email Sent"] ?? false,
-          secondEmailSent: fields["Second Email Sent"] ?? false,
-          unlisted: fields["Unlisted"] ?? false,
-          rsvpCount: fields["RSVP Count"] ?? 0,
-          slug:
-            fields["Custom Slug"] ??
-            slugger.slug(fields["Event Name"] as string),
-          pastEventDesc:
-            fields["Past Event Description"] ??
-            "A past Purdue Hackers event...more details coming soon!",
-          recapImages: fields["Recap Images"] ?? [
-            { url: "https://mbs.zone/geck" },
-          ],
-          hasPastEventDesc:
-            fields["Has Past Event Description?"] === 1 ? true : false,
-          stat1Data: fields["Stat 1 Data"] ?? "",
-          stat1Label: fields["Stat 1 Label"] ?? "",
-          stat2Data: fields["Stat 2 Data"] ?? "",
-          stat2Label: fields["Stat 2 Label"] ?? "",
-          stat3Data: fields["Stat 3 Data"] ?? "",
-          stat3Label: fields["Stat 3 Label"] ?? "",
-        }));
-        resolve(typedEvents as PHEvent[]);
-      })
+      .then((events) => resolve(events))
       .catch((err) => reject(err));
   });
 }
