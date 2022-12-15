d06d21aafcc91d4105808e8e21f1fcb3844a5a30
chi-wei-lien <lien2@purdue.edu>
2022-12-15 12:37:11 -0500

    add cors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import { wishlistTable, wishlistBase } from "../db/wishlist";
 import logger from "../middleware/logger";
@@ -12,6 +13,7 @@ import WishlistController from "../controllers/wishlist";
 const app = express();
 const port = process.env.PORT || "8080";
 
+app.use(cors());
 app.use(logger);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
